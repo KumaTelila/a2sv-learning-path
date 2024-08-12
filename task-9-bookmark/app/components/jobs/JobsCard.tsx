17bf2407f4eb79a951853fda5f96fd7241e1eda3
Kuma Telila <kumatelila26@gmail.com>
2024-08-12 16:31:31 +0300

    toggle button worked

--- task-9-bookmark/app/components/jobs/JobsCard.tsx
+++ task-9-bookmark/app/components/jobs/JobsCard.tsx
@@ -8,6 +8,7 @@ import { FaBookBookmark, FaRegHeart } from "react-icons/fa6";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 import { date } from "zod";
+import { METHODS } from "http";
 const JobsCard = ({
   id,
   title,
@@ -26,14 +27,49 @@ const JobsCard = ({
   orgName: string;
 }) => {
   const session = useSession();
+  const [saved, setSaved] = React.useState(false);
+
+  React.useEffect(() => {
+    const fetchSaved = async () => {
+      if (!session) {
+        return;
+      }
+
+      const accessToken = session?.data?.accessToken;
+      if (!accessToken) {
+        return;
+      }
+
+      try {
+        const res = await fetch(`https://akil-backend.onrender.com/bookmarks`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (res.ok) {
+          const { data } = await res.json();
+          // Check if the current job is saved
+          setSaved(data.some((bookmark: any) => bookmark.eventID === id));
+        } else {
+          console.error("Failed to fetch saved jobs");
+        }
+      } catch (error) {
+        console.error("Error fetching saved jobs:", error);
+      }
+    };
+
+    fetchSaved();
+  }, [session, id]);
   // console.log("this is from job card =========>", session.data?.user?.email);
   const handleClick = async () => {
     const accessToken = session?.data?.accessToken;
-    console.log("this is from job card =========>", session);
-    console.log("this is from job card =========>", accessToken);
+    // console.log("this is from job card =========>", session);
+    // console.log("this is from job card =========>", accessToken);
     if (!accessToken) {
       return;
-
     }
     try {
       const res = await fetch(
@@ -42,7 +78,7 @@ const JobsCard = ({
         {
           method: "POST",
           headers: {
-            "Authorization": `Bearer ${accessToken}`,
+            Authorization: `Bearer ${accessToken}`,
             "Content-Type": "application/json",
           },
           body: JSON.stringify({}),
@@ -50,9 +86,42 @@ const JobsCard = ({
       );
 
       if (res.ok) {
+        setSaved(true);
         console.log("job saved");
       } else {
         console.log("job not saved");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleUnsave = async () => {
+    const accessToken = session?.data?.accessToken;
+    // console.log("this is from job card =========>", session);
+    // console.log("this is from job card =========>", accessToken);
+    if (!accessToken) {
+      return;
+    }
+    try {
+      const res = await fetch(
+        `https://akil-backend.onrender.com/bookmarks/${id}`,
+
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({}),
+        }
+      );
+
+      if (res.ok) {
+        setSaved(false);
+        console.log("job unsaved");
+      } else {
+        console.log("job not unsaved");
       }
     } catch (error) {
       console.log(error);
@@ -77,14 +146,18 @@ const JobsCard = ({
           </Link>
           {session && session.data?.user ? (
             <div className="flex gap-2">
-              <div className="pt-2">
-                <FaRegHeart color="#7C8493" />
-              </div>
               <button
-                onClick={handleClick}
-                className="text-base font-Epilogue text-[#7C8493]"
+                onClick={saved ? handleUnsave : handleClick}
+                className="gap-2 flex text-base font-Epilogue text-[#7C8493]"
               >
-                Save this job
+                <div className="pt-1">
+                  {saved ? (
+                    <FaHeart color="red" />
+                  ) : (
+                    <FaRegHeart color="#7C8493" />
+                  )}
+                </div>
+                <h1>{saved ? "Unsave this job" : "Save this job"}</h1>
               </button>
             </div>
           ) : null}
--- task-9-bookmark/app/layout.tsx
+++ task-9-bookmark/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-import { Inter} from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import StoreProvider from "./StoreProvider";
+import Providers from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
-
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="light">
       <body className={inter.className}>
-        <StoreProvider>{children}</StoreProvider>
+        <Providers>
+          <StoreProvider>{children}</StoreProvider>
+        </Providers>
       </body>
     </html>
   );
